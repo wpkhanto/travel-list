898ab885397aeb96268014d19a26e2b3f1022455
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-20 22:25:33 +0700

    Thinking About State and Lifting State Up

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <States />
         </div>
     );
@@ -21,7 +27,7 @@ function Logo() {
     return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -37,6 +43,8 @@ function Form() {
             id: Date.now(),
         };
         console.log(newItem);
+
+        onAddItems(newItem);
 
         setDescription("");
         setQuantity(1);
@@ -67,11 +75,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item key={item.id} item={item} />
                 ))}
             </ul>
