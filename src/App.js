2a228d99f0e133e8447d99a3915f8fb4ed0a4cfd
Watcharaphon Khanto <wpkhanto@icloud.com>
2024-11-22 19:51:42 +0700

    Updating an Item: Complex Immutable Data Operation

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ export default function App() {
         setItems((items) => items.filter((item) => item.id !== id));
     }
 
+    function handleToggleItem(id) {
+        setItems((items) => 
+            items.map((item) => 
+                item.id === id ? {...item, packed: !item.packed} 
+                : item
+            )
+        );
+    }
+
     function handleAddItems(item) {
         setItems((items) => [...items, item]);
     }
@@ -21,7 +30,7 @@ export default function App() {
         <div className="app">
             <Logo />
             <Form onAddItems={handleAddItems} />
-            <PackingList items={items} onDeleteItem={handleDeleteItem} />
+            <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
             <States />
         </div>
     );
@@ -79,21 +88,22 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
     return (
         <div className="list">
             <ul>
                 {items.map((item) => (
-                    <Item key={item.id} onDeleteItem={onDeleteItem} item={item} />
+                    <Item key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} item={item} />
                 ))}
             </ul>
         </div>
     );
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
     return (
         <li>
+        <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)} />
             <span
                 style={item.packed ? { textDecoration: "line-through" } : null}
             >
